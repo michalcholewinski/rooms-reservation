b3bb2fd01afe71c930487dc49508b3aab35525ed
Michał Cholewiński <cholewinskimichal@gmail.com>
2018-08-18 17:21:19 +0200

    Fixing integration tests

--- src/main/resources/db/migration/V2__insertInitialData.sql
+++ src/main/resources/db/migration/V2__insertInitialData.sql
@@ -3,3 +3,16 @@ INSERT INTO MYBOOKING_ROLE (ID, NAME) VALUES (2, 'CUSTOMER');
 
 INSERT INTO MYBOOKING_USER (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES (1, 'Michal', 'Cholewinski', 'bulko@onet.eu');
 INSERT INTO MYBOOKING_USERS_ROLES (USER_ID, ROLE_ID) VALUES (1, 1);
+
+INSERT INTO MYBOOKING_ROOM(id, number, details, hotel_name, city, price)
+VALUES (nextval('MYBOOKING_ROOM_ID_SEQ'),33,'TV, 2 Windows, Towels available', 'Mariott', 'Gdynia', 300);
+INSERT INTO MYBOOKING_ROOM(id, number, details, hotel_name, city, price)
+VALUES (nextval('MYBOOKING_ROOM_ID_SEQ'),32,'Beautiful room with view to see', 'Mariott', 'Gdynia',2000);
+INSERT INTO MYBOOKING_ROOM(id, number, details, hotel_name, city, price)
+VALUES (nextval('MYBOOKING_ROOM_ID_SEQ'),41,'Room with view to Railway Station', 'Lysogory Hotel', 'Gdynia',500);
+INSERT INTO MYBOOKING_ROOM(id, number, details, hotel_name, city, price)
+VALUES (nextval('MYBOOKING_ROOM_ID_SEQ'),51,'Nothing special, just room with bed', 'Dal Hotel', 'Łodź',550);
+INSERT INTO MYBOOKING_ROOM(id, number, details, hotel_name, city, price)
+VALUES (nextval('MYBOOKING_ROOM_ID_SEQ'),62,'6th floor by stairs. Recommended for old people', 'Dal Hotel', 'Łódź',5500);
+INSERT INTO MYBOOKING_ROOM(id, number, details, hotel_name, city, price)
+VALUES (nextval('MYBOOKING_ROOM_ID_SEQ'),13,'One bad, 3 Bathrooms and welcome drink for guests', 'Matrix Hotel', 'Berlin',1500);
