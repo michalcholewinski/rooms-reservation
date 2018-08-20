fa9b264599d0fb88f44ba62a74eca9f6ae327812
Michał Cholewiński <cholewinskimichal@gmail.com>
2018-08-20 09:31:01 +0200

    Readme file - draft

--- src/main/java/com/mybooking/customers/Customer.java
+++ src/main/java/com/mybooking/customers/Customer.java
@@ -2,10 +2,15 @@ package com.mybooking.customers;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Customer {
     private Long id;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String email;
 }
